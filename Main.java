a5a8f74235934e94fa3681ab6d544c7b0a250bc8
TFrede89 <schokox3.tf@gmail.com>
2026-01-20 13:51:43 +0100

    Main class added incl. logik
    Ticketnr. 1904

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    /**
+     * Main class for main logic
+     * @param args
+     *
+     * erstellt am 20.01.2026
+     */
+    static void main(String[] args) {
+        System.out.println("fetchtest");
+    }
+}
